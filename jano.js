a41c5108c001cab823550be43fa9244d664695ce
xulaup <96148123+xulaup@users.noreply.github.com>
2022-01-25 20:41:25 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -104,7 +104,7 @@ const jano100 = new Discord.Client();
 
 ///////////////////
 jano.on("ready", () => {
-  var join = jano.channels.get(""); /// id voice
+  var join = jano.channels.get("888203180519882772"); /// id voice
   if (join) join.join();
 });
 ///////////////////
@@ -1907,7 +1907,7 @@ jano100.on("ready", () => {
 
 //////////////// tokn lera da bne
 
-jano.login("");
+jano.login("OTM1NTg4MjMwNzE0MjQxMTQ1.YfA1Bw.G26O68gvpc4oLCwfevhZIobtJSI");
 jano2.login("");
 jano3.login("");
 jano4.login("");
